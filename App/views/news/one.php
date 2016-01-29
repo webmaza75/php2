ae2afba1189173b74b217c6698cdec56244e0f3d
webmaza75 <webmaza75@gmail.com>
2016-01-29 09:29:29 +0300

    создан шаблон отображения одной новости

---
+++ App/views/news/one.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Новость</title>
+</head>
+<body>
+    <h1><?php echo $news->title; ?></h1>
+    <div><?php echo $news->content; ?></div>
+</body>
+</html>
