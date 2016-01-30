09bf1dbf6de6954a0f7253d75cb290b6e820a7db
webmaza75 <webmaza75@gmail.com>
2016-01-30 11:27:23 +0300

    создан файл конфигурации с параметрами подключения к БД

---
+++ App/db_config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Файл конфигурации для подключения к БД
+ */
+//$this->dbh = new \PDO('mysql:host=localhost;dbname=php2', 'root', '');
+return [
+    'db' =>[
+        'host' => 'localhost',
+        'dbname' => 'php2',
+        'login' => 'root',
+        'pass' => ''
+    ],
+];
+
