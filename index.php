7a39ea58bd466c35d18ec5fc7b31235591165300
webmaza75 <webmaza75@gmail.com>
2016-01-31 17:13:50 +0300

    index.php

--- index.php
+++ index.php
@@ -5,14 +5,3 @@ require_once __DIR__ . '/autoload.php';
 // Получение трех последних новостей
 $news = App\Models\News::getLast(3);
 include(__DIR__ . './App/views/news/all.php');
-
-/** Альтернативный вариант без использования метода getLast($limit)
-* для сравнительно небольшого числа записей в таблице
-*/
-/*
-$news  = App\Models\News::findAll();
-$limit = 3;
-// Получить срез массива из 3-х элементов ($limit), начиная с конца (-$limit) эл-та
-$news = array_slice($news, -$limit, $limit, true);
-include(__DIR__ . './App/views/news/all.php');
-*/
