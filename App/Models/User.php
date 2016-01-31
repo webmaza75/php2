f5f0ba92587eab54dca225b4b6d0c8c0412b6d31
webmaza75 <webmaza75@gmail.com>
2016-01-31 17:21:44 +0300

    + реализация интерфейса HasEmail

--- App/Models/User.php
+++ App/Models/User.php
@@ -5,10 +5,15 @@ namespace App\Models;
 
 use App\Model;
 
-class User extends Model
+class User extends Model implements HasEmail
 {
     const TABLE = 'users';
 
     public $name;
     public $email;
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
 }
