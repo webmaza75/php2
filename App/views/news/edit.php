ff3b4b47632637b573c8ea86586cf59b7910999e
webmaza75 <webmaza75@gmail.com>
2016-01-31 16:41:47 +0300

    Доработан шаблон редактирования новости

--- App/views/news/edit.php
+++ App/views/news/edit.php
@@ -9,7 +9,7 @@
 <h1>Редактирование новости</h1>
 <form method="post" action="/article.php?id=<?php echo $news->id; ?>">
     <label for="title">Заголовок</label>
-    <input id="title" name="title" type="text" placeholder="Введите заголовок" value="<?php echo $news->title; ?>"required>
+    <input id="title" name="title" type="text" placeholder="Введите заголовок" value="<?php echo $news->title; ?>" required>
     <br>
     <label for="content">Текст</label>
     <textarea id=content" name="content" cols="100" rows="10" placeholder="Введите текст" required><?php echo $news->content; ?></textarea><br>
