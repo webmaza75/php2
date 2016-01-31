b78246c1406fc004d8ec2c64b4f397eb919ecb21
webmaza75 <webmaza75@gmail.com>
2016-01-31 16:53:18 +0300

    Удалена возможность просматривать новость без редактирования

--- article.php
+++ article.php
@@ -1,21 +1,52 @@
 <?php
+
+use \App\Models\News;
+
 require_once __DIR__ . '/autoload.php';
 
-if (isset($_GET['id']) && !empty($_GET['id'])) {
-    $id = intval($_GET['id']);
+$action = isset($_GET['act']) ? $_GET['act'] : false; // Проверка перехода по ссылке
+if ($action && !in_array($action, ['add', 'edit', 'del'])) {
+    header('location: /'); // На главную, если неизвестный $_GET['act']
+}
+if ($action == 'add') { // Если надо добавить новую статью
+    include(__DIR__ . './App/views/news/add.php');
+}
 
-    $news = App\Models\News::findById($id);
+// Если существует $_GET['id']
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+    $news = App\Models\News::findById($id); // поиск новости по id
     if (false === $news) { // для несуществующей новости
         header('location: /'); // возврат на главную страницу
     } else {
-        // Подключение шаблона для одной новости
-        include(__DIR__ . './App/views/news/one.php');
-    }
+        if(isset($_POST['edit'])) {
+            $news->id = $id;
+            $news->title = $_POST['title'];
+            $news->content = $_POST['content'];
+            $news->save();
+            header('location: /'); // возврат на главную страницу
+        }
 
+        if ($action == 'del') {
+            $news->delete();
+            header('location: /'); // возврат на главную страницу
+        } elseif ($action == 'edit') { // Редактирование новости
+            include(__DIR__ . './App/views/news/edit.php');
+        }
+
+    }
 } else {
-    // возврат на главную страницу при отсутствии $_GET['id']
-    header('location: /');
+    if (isset($_POST['add'])) {
+        $news = new News();
+        $news->title = $_POST['title'];
+        $news->content = $_POST['content'];
+        $news->save();
+        header('location: /'); // возврат на главную страницу
+    }
 }
+
+
+
 
 
 
