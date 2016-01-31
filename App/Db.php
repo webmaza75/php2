9a4c93e82fe1efca6b344ade9a49c6b8a1681aa5
webmaza75 <webmaza75@gmail.com>
2016-01-31 16:57:04 +0300

    Добавлена функция lastInsId()

--- App/Db.php
+++ App/Db.php
@@ -3,14 +3,31 @@
 
 namespace App;
 
+use App;
+//use \PDO;
+
 
 class Db
 {
+    // Добавлено использование трейта Singleton
+    use Singleton;
+
     protected $dbh;
 
+    // Используются параметры подключения
     public function __construct()
     {
-        $this->dbh = new \PDO('mysql:host=localhost;dbname=php2', 'root', '');
+        $config = App\Config::instance();
+
+        $param1 = 'mysql:host=' .
+            $config->data['db']['host'] .
+            ';dbname=' .
+            $config->data['db']['dbname'];
+
+        $param2 = $config->data['db']['login'];
+        $param3 = $config->data['db']['pass'];
+
+        $this->dbh = new \PDO($param1, $param2, $param3);
     }
 
     public function execute($sql, $args = [])
@@ -38,5 +55,11 @@ class Db
             return $sth->fetchAll(\PDO::FETCH_CLASS, $class);
         }
         return [];
+    }
+
+    // Получаем id последней вставленной в БД записи
+    public function lastInsId()
+    {
+        return $this->dbh->lastInsertId();
     }
 }
