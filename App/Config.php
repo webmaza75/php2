d5cc11316fcb23f592b0cf65fec95e89791840ea
webmaza75 <webmaza75@gmail.com>
2016-02-01 16:38:57 +0300

    + упрощен код (+ use Singleton)

--- App/Config.php
+++ App/Config.php
@@ -10,19 +10,11 @@ namespace App;
 
 class Config
 {
+    use Singleton;
     public $data = [];
-    protected static $instance;
 
     protected function __construct()
     {
         $this->data = include_once __DIR__ . '/db_config.php';
-    }
-
-    public static function instance()
-    {
-        if (is_null(self::$instance)) {
-            self::$instance = new static;
-        }
-        return self::$instance;
     }
 }
