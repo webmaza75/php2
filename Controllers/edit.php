2dbe40f51c66a4788367495fed4d76f86e356f86
webmaza75 <webmaza75@gmail.com>
2016-02-02 17:21:22 +0300

    изменен подход: общий контроллер при добавлении или редактирования новости ведет на страницу с формой редактирования

--- Controllers/edit.php
+++ Controllers/edit.php
@@ -1,22 +1,13 @@
 <?php
 
+use App\Models\News;
+
 require_once __DIR__ . '/../autoload.php';
 
-if (isset($_POST['title']) && isset($_POST['content'])) {
-    $title = trim($_POST['title']);
-    $content = trim($_POST['content']);
-    if ($title != '' && $content != '') {
-        $news = App\Models\News::findById((int)$_POST['id']);
-        $news->title = $title;
-        $news->content = $content;
-        if ($news->save()) {
-            header('location: /');
-            exit;
-        }
-    }
-}
-if (isset($_GET['id'])) {
+if (!isset($_GET['id'])) {
     $id = (int)$_GET['id'];
-    $news = App\Models\News::findById($id);
-    include(__DIR__ . '/../App/views/news/edit.php');
+    $news = new News();
+} else {
+    $news = App\Models\News::findById((int)$_GET['id']);
 }
+include(__DIR__ . '/../App/views/news/edit.php');
