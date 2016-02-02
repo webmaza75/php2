f25e3e3bd0afee1d3f2c96623338112d723f6cab
webmaza75 <webmaza75@gmail.com>
2016-02-02 16:19:29 +0300

    id передается через $_POST

--- Controllers/edit.php
+++ Controllers/edit.php
@@ -2,25 +2,21 @@
 
 require_once __DIR__ . '/../autoload.php';
 
-if (isset($_GET['id'])) {
-    $id = (int)$_GET['id'];
-    $news = App\Models\News::findById($id);
-}
-
-if (isset($_GET['act']) && $_GET['act'] == 'edit') {
-    include(__DIR__ . './../App/views/news/edit.php');
-} else {
-    header('location: /'); // На главную, если неизвестный $_GET['act']
-}
-
 if (isset($_POST['edit']) && isset($_POST['title']) && isset($_POST['content'])) {
     $title = trim($_POST['title']);
     $content = trim($_POST['content']);
-
     if ($title != '' && $content != '') {
+        $news = App\Models\News::findById((int)$_POST['id']);
         $news->title = $title;
         $news->content = $content;
-        $news->save();
-        header('location: /'); // возврат на главную страницу
+        if ($news->save()) {
+            header('location: /');
+            exit;
+        }
     }
+}
+if (isset($_GET['id'])) {
+    $id = (int)$_GET['id'];
+    $news = App\Models\News::findById($id);
+    include(__DIR__ . '/../App/views/news/edit.php');
 }
