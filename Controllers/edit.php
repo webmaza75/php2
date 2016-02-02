51ab9309eef515be29a7c5d31b87c2ceeed91c3c
webmaza75 <webmaza75@gmail.com>
2016-02-02 17:42:52 +0300

    исправлена ошибка

--- Controllers/edit.php
+++ Controllers/edit.php
@@ -5,9 +5,9 @@ use App\Models\News;
 require_once __DIR__ . '/../autoload.php';
 
 if (!isset($_GET['id'])) {
-    $id = (int)$_GET['id'];
     $news = new News();
 } else {
+    $id = (int)$_GET['id'];
     $news = App\Models\News::findById((int)$_GET['id']);
 }
 include(__DIR__ . '/../App/views/news/edit.php');
