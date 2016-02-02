b3ad5031852165b21396cf714da73e810f8f52f9
webmaza75 <webmaza75@gmail.com>
2016-02-02 13:45:09 +0300

    сокращен код

--- Controllers/del.php
+++ Controllers/del.php
@@ -1,14 +1,11 @@
 <?php
 require_once __DIR__ . '/../autoload.php';
 
-if (isset($_GET['act']) && $_GET['act'] == 'del') {
-
-    if(isset($_GET['id'])) {
-        $id = intval($_GET['id']);
-        $news = App\Models\News::findById($id); // поиск новости по id
-        if (false !== $news) {
-            $news->delete();
-        }
+if(isset($_GET['id'])) {
+    $news = App\Models\News::findById((int)$_GET['id']); // поиск новости по id
+    if (false !== $news) {
+        $news->delete();
     }
-    header('location: /'); // возврат на главную страницу
 }
+header('location: /'); // возврат на главную страницу
+exit;
