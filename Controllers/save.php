37298a6c15efe3bf33e45c50c33d8a2401f82748
webmaza75 <webmaza75@gmail.com>
2016-02-02 17:31:08 +0300

    общий контроллер для сохранения и обновления данных новости, полученных из формы от пользователя

---
+++ Controllers/save.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\News;
+
+require_once __DIR__ . '/../autoload.php';
+
+if (isset($_POST['id'])) {
+    $news = App\Models\News::findById((int)$_POST['id']);
+} else {
+    $news = new News();
+}
+
+$news->title = $_POST['title'];
+$news->content = $_POST['content'];
+if ($news->save()) {
+    header('location: /');
+    exit;
+}
