4054577402d3493047f6c804c8da3ebfa33d4a03
webmaza75 <webmaza75@gmail.com>
2016-02-02 13:45:56 +0300

    доработан код

--- Controllers/add.php
+++ Controllers/add.php
@@ -1,24 +1,30 @@
 <?php
-
+/**
+ * 1. Проверьте, что от пользователя что-то пришло
+ * 2. ЕСЛИ пришло нужное, ТО попытайтесь создать и сохранить модель
+ * 3. ЕСЛИ пункт 2. удался - то перенаправьте пользователя куда-нибудь, хоть на ту же страницу.
+ * Не забудьте остановить программу после перенаправления!
+ * 4. А вот тут покажите форму
+ *******
+ * - edit(NEW)
+ * - edit(id=NNN)
+ * - save()
+ */
 use App\Models\News;
 
 require_once __DIR__ . '/../autoload.php';
 
-if (isset($_GET['act']) && $_GET['act'] == 'add') {
-    include(__DIR__ . './../App/views/news/add.php');
-} else {
-    header('location: /'); // На главную, если неизвестный $_GET['act']
-}
-
 if (isset($_POST['add']) && isset($_POST['title']) && isset($_POST['content'])) {
     $title = trim($_POST['title']);
     $content = trim($_POST['content']);
-
     if ($title != '' && $content != '') {
         $news = new News();
         $news->title = $title;
         $news->content = $content;
-        $news->save();
-        header('location: /'); // возврат на главную страницу
+        if ($news->save()) {
+            header('location: /');
+            exit;
+        }
     }
 }
+include(__DIR__ . '/../App/views/news/add.php');
