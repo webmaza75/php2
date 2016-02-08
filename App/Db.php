ac2aa80547e0c99944a32b9c80cc56e73a1aa8be
Irina Rabinovich <webmaza75@gmail.com>
2016-02-08 12:01:23 +0300

    + PHPDoc

--- App/Db.php
+++ App/Db.php
@@ -4,17 +4,26 @@
 namespace App;
 
 use App;
-//use \PDO;
 
-
+/**
+ * Class Db класс подключения к БД
+ * @package App
+ */
 class Db
 {
-    // Добавлено использование трейта Singleton
+    /**
+     * Использование трейта Singleton для поддержания единственного соединения с БД
+     */
     use Singleton;
 
+    /**
+     * @var \PDO - объект PDO, предоставляющий соединение с БД
+     */
     protected $dbh;
 
-    // Используются параметры подключения
+    /**
+     * Создание объекта подключения к БД
+     */
     public function __construct()
     {
         $config = App\Config::instance();
@@ -30,6 +39,12 @@ class Db
         $this->dbh = new \PDO($param1, $param2, $param3);
     }
 
+    /**
+     * Метод выполнения запроса к БД на добавление|обновление|удаление записи об объекте
+     * @param string $sql строка запроса
+     * @param array $args массив подстановок в строку запроса
+     * @return bool
+     */
     public function execute($sql, $args = [])
     {
         $sth = $this->dbh->prepare($sql);
@@ -41,6 +56,13 @@ class Db
         return $res;
     }
 
+    /**
+     * Метод выполнения запроса на получение данных из БД
+     * @param string $class имя класса, к которому приводится объект из результирующего набор данных
+     * @param string $sql строка запроса к БД
+     * @param array $args массив подстановок для подготовленного запроса
+     * @return array
+     */
     public function query($class, $sql, $args = [])
     {
         $sth = $this->dbh->prepare($sql);
@@ -51,13 +73,16 @@ class Db
         }
 
         if(false !== $res) {
-            // Возвращается объект нужного класса
+            // Возвращается масисв с объектом|объектами нужного класса
             return $sth->fetchAll(\PDO::FETCH_CLASS, $class);
         }
         return [];
     }
 
-    // Получаем id последней вставленной в БД записи
+    /**
+     * Получаем id последней вставленной в БД записи
+     * @return string
+     */
     public function lastInsId()
     {
         return $this->dbh->lastInsertId();
