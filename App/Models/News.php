bd96e675f5b4e53128d380ae079b38aab5b6517f
Irina Rabinovich <webmaza75@gmail.com>
2016-02-08 11:51:29 +0300

    + PHPDoc, use трейт MagicFunc, метод получения имени автора по его id

--- App/Models/News.php
+++ App/Models/News.php
@@ -2,14 +2,44 @@
 
 namespace App\Models;
 
-
+use App\MagicFunc;
 use App\Model;
+use App\Db;
 
+/**
+ * Class News класс новости
+ * @package App\Models
+ */
 class News extends Model
 {
+    /**
+     * Используется трейт с магическими функциями
+     */
+    use MagicFunc;
+    /**
+     * Table name имя таблицы
+     */
     const TABLE = 'news';
 
-    public $id;
-    public $title;
-    public $content;
+    /**
+     * Метод получения имени автора новости по его id
+     * @param integer $authorId Id of the author
+     * @return mixed (bool|string)
+     */
+    public function getAuthor($authorId)
+    {
+        if (empty($authorId)) {
+            return false;
+        }
+        $id = (int)$authorId;
+        $db = Db::instance();
+        $author = new Author();
+        $args = [':id' => $id];
+        $sql = 'SELECT * FROM ' . $author::TABLE . ' WHERE `id`=:id;';
+        $res = $db->query('\App\Models\Author', $sql, $args);
+        if (!$res) {
+            return false;
+        }
+        return $res[0]->name;
+    }
 }
