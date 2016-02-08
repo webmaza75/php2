1ec4fb757ed44d3ccd583f6a7b7e9a77feb8ceed
Irina Rabinovich <webmaza75@gmail.com>
2016-02-08 12:07:38 +0300

    + реализация ArrayAccess, удалено использование магических методов, изменен метод render

---
+++ App/View.php
@@ -0,0 +1,101 @@
+<?php
+
+
+namespace App;
+
+/**
+ * Class View класс представления
+ * @package App
+ */
+class View implements \Countable, \ArrayAccess
+{
+    protected $data =[];
+    /**
+     * Использует трейт с магическими функциями,
+     * трейт использовался до реализации интерфейса ArrayAccess
+     */
+    //use MagicFunc;
+
+    /**
+     * Формирование контента страницы для вывода в браузере
+     * @param string $template путь к шаблону страницы
+     * @return string содержимое (контент) страницы для вывода в браузере
+     */
+    public function render($template)
+    {
+        ob_start();
+        /*
+        foreach ($this->data as $prop => $value) {
+            $$prop = $value;
+        }
+        */
+        foreach ($this as $prop => $value) {
+            $$prop = $value;
+        }
+        include $template;
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
+    }
+
+    /**
+     * Вывод страницы в браузере для пользователя
+     * @param string $template путь к шаблону страницы
+     */
+    public function display($template)
+    {
+        echo $this->render($template);
+    }
+
+    /**
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * The return value is cast to an integer.
+     * @since 5.1.0
+     */
+    public function count()
+    {
+        return count($this->data);
+    }
+
+    /**
+     * Устанавливает заданное смещение (ключ)
+     * @param mixed $offset
+     * @param mixed $value
+     */
+    public function offsetSet($offset, $value) {
+        if (is_null($offset)) {
+            $this->data[] = $value;
+        } else {
+            $this->data[$offset] = $value;
+        }
+    }
+
+    /**
+     * Определяет, существует ли заданное смещение (ключ)
+     * @param mixed $offset
+     * @return bool
+     */
+    public function offsetExists($offset) {
+        return isset($this->data[$offset]);
+    }
+
+    /**
+     * Удаляет смещение
+     * @param mixed $offset
+     */
+    public function offsetUnset($offset) {
+        unset($this->data[$offset]);
+    }
+
+    /**
+     * Возвращает заданное смещение (ключ)
+     * @param mixed $offset
+     * @return null
+     */
+    public function offsetGet($offset) {
+        return isset($this->data[$offset]) ? $this->data[$offset] : null;
+    }
+}
+
