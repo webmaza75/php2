270618306f9ba304c52562c90250413c6f557f92
Irina Rabinovich <webmaza75@gmail.com>
2016-02-08 11:52:04 +0300

    + PHPDoc

--- App/Models/User.php
+++ App/Models/User.php
@@ -5,13 +5,34 @@ namespace App\Models;
 
 use App\Model;
 
+/**
+ * Class User
+ * @package App\Models
+ * @var string $name User name
+ * @var string $email email address
+ */
+
 class User extends Model implements HasEmail
 {
+    /**
+     * Имя таблицы в БД
+     */
     const TABLE = 'users';
 
+    /**
+     * @var string $name Имя пользователя
+     */
     public $name;
+
+    /**
+     * @var string $email Адрес email
+     */
     public $email;
 
+    /**
+     * Реализация метода интерфейса HasEmail получение адреса email объекта
+     * @return string Email address
+     */
     public function getEmail()
     {
         return $this->email;
