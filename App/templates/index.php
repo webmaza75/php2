f921dcb27346ace2a76928502106495c094785c8
Irina Rabinovich <webmaza75@gmail.com>
2016-02-08 11:55:33 +0300

    + вывод всех пользователей

---
+++ App/templates/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title><?php echo $title; ?></title>
+
+    <!-- Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<h1>Пользователи</h1>
+
+<?php foreach ($users as $user) : ?>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <?php if (!empty($user->name)) : ?>
+                <?php echo $user->name; ?>
+            <?php else : ?>
+                -= Без имени =-
+            <?php endif; ?>
+        </div>
+        <div class="panel-body"><?php echo $user->email; ?></div>
+    </div>
+
+<?php endforeach; ?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
