e741e30cbaea0f53012eb52ddcf87f3dc7fdbcd7
Irina Rabinovich <webmaza75@gmail.com>
2016-02-08 12:05:42 +0300

    + PHPDoc

--- App/Singleton.php
+++ App/Singleton.php
@@ -5,15 +5,29 @@
 
 namespace App;
 
-
+/**
+ * Class Singleton трейт Singleton
+ * @package App
+ */
 trait Singleton
 {
+    /**
+     * @var object $instance статическое свойство класса,
+     * может содержать единственный объект данного класса
+     */
     protected static $instance;
 
+    /**
+     * Защищенный конструктор
+     */
     protected function __construct()
     {
     }
 
+    /**
+     * Статический метод возвращает единственный объект
+     * @return object
+     */
     public static function instance()
     {
         // Если переменная не имеет значения (null), т.е. не существует объект static
