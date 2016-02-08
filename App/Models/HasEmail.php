3049e485451897a7da1559a04d04cd69c9af61d1
Irina Rabinovich <webmaza75@gmail.com>
2016-02-08 11:49:15 +0300

    + PHPDoc

--- App/Models/HasEmail.php
+++ App/Models/HasEmail.php
@@ -2,8 +2,15 @@
 
 namespace App\Models;
 
-
+/**
+ * Interface HasEmail
+ * @package App\Models
+ *
+ */
 interface HasEmail
 {
+    /**
+     * @return string Email address
+     */
     public function getEmail();
 }
