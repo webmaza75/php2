d300ea6d9eac1f82e050dfce7b0c72344423491a
Irina Rabinovich <webmaza75@gmail.com>
2016-02-08 12:02:09 +0300

    + трейт с магическими функциями

---
+++ App/MagicFunc.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App;
+
+/**
+ * Class MagicFunc (trait)
+ * @package App
+ */
+trait MagicFunc
+{
+    /**
+     * @var array $data Array of Properties
+     */
+    protected $data = [];
+
+    /**
+     * Магический метод добавления несуществующего свойства объекта
+     * @param mixed $k Property name
+     * @param mixed $v Property value
+     */
+    public function __set($k, $v)
+    {
+        $this->data[$k] = $v;
+    }
+
+    /**
+     * Магический метод возвращения значения свойства объекта по его имени (ключу)
+     * @param mixed $k Property name
+     * @return mixed
+     */
+    public function __get($k)
+    {
+        return $this->data[$k];
+    }
+
+    /**
+     * Проверка на существование свойства объекта по его имени (ключу)
+     * @param mixed $k Property name
+     * @return bool
+     */
+    public function __isset($k)
+    {
+        return isset($this->data[$k]);
+    }
+}
