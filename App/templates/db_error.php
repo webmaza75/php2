45964eb5a8640ccb3e8131a596484dbda049eb79
Irina Rabinovich <webmaza75@gmail.com>
2016-02-19 15:34:57 +0300

    убрала <script src="js/bootstrap.min.js"></script> в шаблонах, т.к. этого файла на диске нет, но в фоне файл пытается подключиться, что выдает ошибку отсутствия файла, это сообщение пишется в лог

--- App/templates/db_error.php
+++ App/templates/db_error.php
@@ -26,7 +26,5 @@
 </div>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
--- App/templates/one.php
+++ App/templates/one.php
@@ -48,7 +48,5 @@
 </div>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
