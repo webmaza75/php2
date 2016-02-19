d1ca1d410f593117e58227ccc3b8514db807c2f2
Irina Rabinovich <webmaza75@gmail.com>
2016-02-19 15:46:23 +0300

    изменен mod_rewrite + ( ! -l )

--- App/log.php
+++ App/log.php
@@ -1,2 +1,5 @@
-19-02-2016 13:02:23 |Несуществующий адрес $_SERVER['REQUEST_URI']:/admin/js/bootstrap.min.js| $ctrl: \App\Controllers\adminЭкшен: actionjs| [file]: D:\OpenServer\domains\php2.local\index.php; | [line]: 35; | [code]: 0;
-19-02-2016 13:02:23 |Неверный адрес $_SERVER['REQUEST_URI']:/favicon.ico| $ctrl: \App\Controllers\favicon.ico| [file]: D:\OpenServer\domains\php2.local\index.php; | [line]: 29; | [code]: 0;
+19-02-2016 15:02:14 |Такой автор не существует| [file]: D:\OpenServer\domains\php2.local\App\Models\News.php; | [line]: 113; | [code]: 0;
+19-02-2016 15:02:19 |Пустое поле title| [file]: D:\OpenServer\domains\php2.local\App\Models\News.php; | [line]: 107; | [code]: 0;
+19-02-2016 15:02:19 |Такой автор не существует| [file]: D:\OpenServer\domains\php2.local\App\Models\News.php; | [line]: 113; | [code]: 0;
+19-02-2016 15:02:32 |Пустое поле content| [file]: D:\OpenServer\domains\php2.local\App\Models\News.php; | [line]: 107; | [code]: 0;
+19-02-2016 15:02:54 |Статья не найдена| [file]: D:\OpenServer\domains\php2.local\App\Controllers\News.php; | [line]: 25; | [code]: 0;
