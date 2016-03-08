37999d14e434ea3d4b7d6a8ac3713ace22d210a2
Irina Rabinovich <webmaza75@gmail.com>
2016-03-08 09:52:17 +0300

    изменена автозагрузка

--- autoload.php
+++ autoload.php
@@ -2,7 +2,7 @@
 /*
  * Файл автозагрузки
  */
-
+/*
 function my_autoload($class)
 {
     $class = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
@@ -15,4 +15,5 @@ function my_autoload($class)
 }
 
 spl_autoload_register('my_autoload');
+*/
 include_once __DIR__ . '/vendor/autoload.php';
